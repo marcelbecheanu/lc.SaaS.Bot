64b199ca65aa9b202c94467deddb5a630711253b
Marcel Becheanu <marcel.becheanu@outlook.pt>
2023-03-15 01:04:48 +0000

    feat(index): Adding optimizations.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 console.clear();
 
-import fs from 'fs';
+import express from 'express';
+import compression from 'compression';
+
+const app = express();
+
+app.use(compression());
+app.use(express.json());
+
+
+app.listen(3000, () => {
+  console.log(`API rodando na porta ${3000}`);
+});
